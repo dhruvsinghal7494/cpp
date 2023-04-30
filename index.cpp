04b16efa11f898139e11da4efcc419f055636ab1
dhruvsinghal7494 <122340013+dhruvsinghal7494@users.noreply.github.com>
2023-04-30 14:47:18 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int main(){
-    cout<<"Hello World";
+    // cout<<"Hello World";
+    double x= 48956123;
+    int y;
+    double z = x%(pow(double 10,double 4));
+    y=log10(x)+1;
+    cout<<y;
+    cout<<z;
+
     return 0;
 }
